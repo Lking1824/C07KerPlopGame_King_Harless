dec30d106096a29058194b3fee725cdc585ff4ad
Lking1824 <king1@mymail.mines.edu>
2022-02-06 21:28:16 -0700

    Created Project

---
+++ src/gameEngine/InteractionResult.java
@@ -0,0 +1,15 @@
+// Lucas King & Mason Harless
+// C07A KerPlop Game
+
+package gameEngine;
+
+/**
+ * Interacting with a game piece must yield one of these results.
+ * 
+ * @author Mark Baldwin
+ * @author Cyndi Rader
+ * 
+ */
+public enum InteractionResult {
+	HIT, KILL, ADVANCE, GET_POINT, NONE;
+}
---
+++ src/levelPieces/LevelSetup.java
@@ -0,0 +1,8 @@
+// Lucas King & Mason Harless
+// C07A KerPlop Game
+
+package levelPieces;
+
+public class LevelSetup {
+
+}
